8aa40b07456e30f6e2450a615831a79af0393320
Janessa Garrow <janessa.garrow@gmail.com>
2020-03-21 11:54:03 -0600

    Create action CRUD methods

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 Go code!
 */
 
+const validateProjectId = require("./middleware/validateProjectId");
 const express = require("express");
 const logger = require("morgan");
 // const cors = require("cors");
-// const projects = "./data/lambda.db3";
 const projects = require("./data/helpers/projectModel");
+// const actionRoutes = require("./routes/actionRoutes");
+const actions = require("./data/helpers/actionModel");
 
 const server = express();
 ("");
@@ -25,23 +27,7 @@ const port = 5000;
 const baseUrl = "/";
 
 server.use(express.json());
-
-const validateUserId = async (req, res, next) => {
-    const { id } = req.params;
-    console.log("id in validateUserId: ", id);
-
-    const projectsArray = await projects.get();
-    console.log("projectsArray: ", projectsArray);
-    const projectIndex = projectsArray.findIndex(project => project.id === id);
-    console.log("projectIndex: ", projectIndex);
-
-    if (projectIndex > -1) {
-        req.user = projectsArray[projectIndex];
-        next();
-    } else {
-        res.status(400).json({ errorMessage: "Invalid user id" });
-    }
-};
+// server.use(`${baseUrl}/actions`, actionRoutes);
 
 server.get("/", (req, res) => {
     projects
@@ -56,7 +42,7 @@ server.get("/", (req, res) => {
         );
 });
 
-server.get("/:id", (req, res) => {
+server.get("/:id", validateProjectId, (req, res) => {
     const { id } = req.params;
 
     projects
@@ -90,7 +76,7 @@ server.post("/", (req, res) => {
     }
 });
 
-server.put("/:id", (req, res) => {
+server.put("/:id", validateProjectId, (req, res) => {
     const { id } = req.params;
     const updatedProject = req.body;
 
@@ -98,9 +84,7 @@ server.put("/:id", (req, res) => {
         .update(id, updatedProject)
         .then(response => {
             response
-                ? res
-                      .status(200)
-                      .json(`Successfully updated project with id ${id}`)
+                ? res.status(200).send(response)
                 : res.status(404).json({
                       errorMessage: `Project with id ${id} does not exist`
                   });
@@ -112,28 +96,107 @@ server.put("/:id", (req, res) => {
         );
 });
 
-server.delete("/:id", (req, res) => {
+server.delete("/:id", validateProjectId, (req, res) => {
     const { id } = req.params;
 
     projects
         .remove(id)
         .then(resp => {
             resp > 0
-                ? res
-                      .status(200)
-                      .json({
-                          message: `Project ${id} was successfully deleted`
-                      })
-                : res
-                      .status(400)
-                      .json({
-                          errorMessage: `No project with id ${id} was found`
-                      });
+                ? res.status(200).json({
+                      message: `Project ${id} was successfully deleted`
+                  })
+                : res.status(400).json({
+                      errorMessage: `No project with id ${id} was found`
+                  });
         })
         .catch(err =>
             res
                 .status(500)
                 .json({ errorMessage: `Error deleting project ${id}` })
+        );
+});
+
+server.get("/:id/actions", validateProjectId, (req, res) => {
+    actions
+        .get()
+        .then(resp => res.status(200).send(resp))
+        .catch(err =>
+            res.status(500).json({ errorMessage: "Error retrieving actions" })
+        );
+});
+
+server.post("/:id/actions", validateProjectId, (req, res) => {
+    const { id } = req.params;
+    const projectAction = { ...req.body, project_id: id };
+
+    if (!projectAction || Object.keys(projectAction).length === 0) {
+        res.status(400).json({ errorMessage: `Missing action data` });
+    } else if (!projectAction.description || !projectAction.notes) {
+        res.status(400).json({
+            errorMessage: `Please include a description and notes`
+        });
+    } else if (projectAction.description.length > 128) {
+        res.status(400).json({
+            errorMessage: `Action description must be 128 characters or less`
+        });
+    } else {
+        actions
+            .insert(projectAction)
+            .then(resp => res.status(200).send(resp))
+            .catch(err =>
+                res.status(500).json({
+                    errorMessage: `Error creating new action for project ${id}`
+                })
+            );
+    }
+});
+
+server.put("/:id/actions/:action_id", validateProjectId, (req, res) => {
+    const { id, action_id } = req.params;
+
+    const updatedAction = req.body;
+
+    if (updatedAction.description && updatedAction.description.length > 128) {
+        res.status(400).json({
+            errorMessage: "Description must be 128 characters or less"
+        });
+    } else {
+        actions
+            .update(action_id, updatedAction)
+            .then(response => {
+                response
+                    ? res.status(200).send(response)
+                    : res.status(404).json({
+                          errorMessage: `Action with id ${action_id} does not exist`
+                      });
+            })
+            .catch(err =>
+                res.status(500).json({
+                    errorMessage: `Error updating action ${action_id}`
+                })
+            );
+    }
+});
+
+server.delete("/:id/actions/:action_id", validateProjectId, (req, res) => {
+    const { action_id } = req.params;
+
+    actions
+        .remove(action_id)
+        .then(resp => {
+            resp > 0
+                ? res.status(200).json({
+                      message: `Action ${action_id} was successfully delete`
+                  })
+                : res.status(400).json({
+                      errorMessage: `No action with id ${action_id} was found`
+                  });
+        })
+        .catch(err =>
+            res
+                .status(500)
+                .json({ errorMessage: `Error deleting action ${action_id}` })
         );
 });
 
